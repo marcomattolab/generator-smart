33d5cb7f3b75c1cf7b592c9f58eac61d41c3aa47
marcomatto <marcomatto@gmail.com>
2020-01-21 11:25:30 +0100

    Added profiling and generation liquibase CSV for Users, Authorities and U/A tables

---
+++ src/main/java/it/eng/generate/template/resouces/TemplateLiquidbaseUsersAuthoritiesCSV.java
@@ -0,0 +1,66 @@
+package it.eng.generate.template.resouces;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import it.eng.generate.ConfigCreateProject;
+import it.eng.generate.DataBase;
+import it.eng.generate.Utils;
+import it.eng.generate.template.AbstractResourceTemplate;
+
+public class TemplateLiquidbaseUsersAuthoritiesCSV extends AbstractResourceTemplate{
+	
+	public TemplateLiquidbaseUsersAuthoritiesCSV(DataBase database) {
+		super(database);
+	}
+	
+	public String getTypeTemplate() {
+		String typeTemplate = Utils.replace(ConfigCreateProject.getIstance().getResConfigLiquidbaseFolder(),".","/");
+		return typeTemplate;
+	}
+	
+	public String getTypeFile() {
+		return "csv";
+	}
+
+	public String getBody() {
+		// https://www.buildmystring.com/
+		ConfigCreateProject conf = ConfigCreateProject.getIstance();
+		
+		String[] profiles = conf.getProfiles();
+		String userProfileList  = profiles!=null && profiles.length>0 
+								? buildProfiles(profiles) 
+								: buildDefaultProfiles();
+							
+		String body = "user_id;authority_name\n" + userProfileList; 					
+		return body;
+	}
+
+
+	private String buildDefaultProfiles() {
+		String res= "1;ROLE_ADMIN\n" + 
+					"1;ROLE_USER\n" + 
+					"3;ROLE_ADMIN\n" + 
+					"3;ROLE_USER\n" + 
+					"4;ROLE_USER\n";
+		return res;
+	}
+
+	private String buildProfiles(String[] profiles) {
+		String res = "";
+		for (int id = 0; id < profiles.length; id++) {
+			String profileRole = profiles[id];
+			//"1;ROLE_ADMIN\n" +
+			//"2;ROLE_USER\n" + 
+			res+= id+";"+profileRole+"\n";
+		}
+		return res;
+	}
+
+	public String getClassName() {
+		return "authorities";
+	}
+	
+	public String getSourceFolder() {
+		return "src/main/resources";
+	}
+
+}
